9ef5859c67b17dffa3e9cc8d0ce9610fd8c68d43
Michael Stanclift <mx@vmstan.com>
2025-02-20 11:16:57 -0600

    Refactor SQL creation script: update table and column names for consistency

--- creation.sql
+++ creation.sql
@@ -1,55 +1,55 @@
 CREATE TABLE
-  "BadTLD" ("TLD" TEXT PRIMARY KEY);
+  bad_tld (tld TEXT PRIMARY KEY);
 
 CREATE TABLE
-  "ErrorLog" (
-    "Event" SERIAL PRIMARY KEY,
-    "Timestamp" TIMESTAMP
+  error_log (
+    event SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP
     WITH
       TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-      "Domain" TEXT DEFAULT NULL,
-      "Error" TEXT DEFAULT NULL
+      domain TEXT DEFAULT NULL,
+      error TEXT DEFAULT NULL
   );
 
 CREATE TABLE
-  "JunkWords" ("Keywords" TEXT PRIMARY KEY);
+  junk_words (keywords TEXT PRIMARY KEY);
 
 CREATE TABLE
-  "MastodonDomains" (
-    "Domain" TEXT PRIMARY KEY,
-    "SoftwareVersion" TEXT DEFAULT NULL,
-    "TotalUsers" INTEGER DEFAULT NULL,
-    "ActiveUsersMonthly" INTEGER DEFAULT NULL,
-    "Timestamp" TEXT DEFAULT NULL,
-    "CVEPatch" BOOLEAN DEFAULT NULL,
-    "Contact" TEXT DEFAULT NULL,
-    "Source" TEXT DEFAULT NULL,
-    "FullVersion" TEXT DEFAULT NULL
+  mastodon_domains (
+    domain TEXT PRIMARY KEY,
+    software_version TEXT DEFAULT NULL,
+    total_users INTEGER DEFAULT NULL,
+    active_users_monthly INTEGER DEFAULT NULL,
+    timestamp TEXT DEFAULT NULL,
+    cve_patch BOOLEAN DEFAULT NULL,
+    contact TEXT DEFAULT NULL,
+    source TEXT DEFAULT NULL,
+    full_version TEXT DEFAULT NULL
   );
 
 CREATE TABLE
-  "NoPeers" ("Domain" TEXT PRIMARY KEY);
+  no_peers (domain TEXT PRIMARY KEY);
 
 CREATE TABLE
-  "RawDomains" (
-    "Domain" TEXT PRIMARY KEY,
-    "Failed" INTEGER DEFAULT NULL,
-    "Ignore" INTEGER DEFAULT NULL,
-    "Errors" INTEGER DEFAULT NULL,
-    "Reason" TEXT DEFAULT NULL,
-    "NXDOMAIN" INTEGER DEFAULT NULL,
-    "Robots" INTEGER DEFAULT NULL
+  raw_domains (
+    domain TEXT PRIMARY KEY,
+    failed BOOLEAN DEFAULT NULL,
+    ignore BOOLEAN DEFAULT NULL,
+    errors INTEGER DEFAULT NULL,
+    reason TEXT DEFAULT NULL,
+    nxdomain BOOLEAN DEFAULT NULL,
+    norobots BOOLEAN DEFAULT NULL
   );
 
 INSERT INTO
-  "BadTLD" ("TLD")
+  bad_tld (tld)
 VALUES
   ('gov'),
   ('mil'),
   ('su');
 
 INSERT INTO
-  "JunkWords" ("Keywords")
+  junk_words (keywords)
 VALUES
   ('activitypub-proxy.cf'),
   ('activitypub-troll.cf'),
