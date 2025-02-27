ecf703464ab197facf534966537c0ba6f896e152
Michael Stanclift <mx@vmstan.com>
2025-02-27 09:27:06 -0600

    Update nightly version ranges in mstmap.py to adjust end dates

--- mstmap.py
+++ mstmap.py
@@ -7,8 +7,8 @@ backport_branches = ["4.3", "4.2", "4.1"]
 
 # Define nightly version ranges with their respective start and end dates
 nightly_version_ranges = [
-    ("4.4.0-alpha.3", datetime(2025, 2, 27), datetime(2026, 12, 31)),
-    ("4.4.0-alpha.2", datetime(2025, 1, 16), datetime(2025, 2, 26)),
+    ("4.4.0-alpha.3", datetime(2025, 2, 28), datetime(2026, 12, 31)),
+    ("4.4.0-alpha.2", datetime(2025, 1, 16), datetime(2025, 2, 27)),
     ("4.4.0-alpha.1", datetime(2024, 10, 8), datetime(2025, 1, 15)),
     ("4.3.0-rc.1", datetime(2024, 10, 2), datetime(2024, 10, 7)),
     ("4.3.0-beta.2", datetime(2024, 9, 18), datetime(2024, 10, 1)),
