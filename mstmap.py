39db5997c536b5032bf6e5757f1495b388390719
Michael Stanclift <mx@vmstan.com>
2025-03-13 11:01:39 -0500

    Update nightly version ranges in mstmap.py to correct release dates

--- mstmap.py
+++ mstmap.py
@@ -6,16 +6,16 @@ from datetime import datetime
 backport_branches = ["4.3", "4.2", "4.1"]
 
 # Define nightly version ranges with their respective start and end dates
-# First date is the state of release in UTC, last is the previous day, with most recent in the future
+# First date is the date on the -security release or the first nightly
 nightly_version_ranges = [
-    ("4.4.0-alpha.4", datetime(2025, 3, 13), datetime(2026, 12, 31)),
-    ("4.4.0-alpha.3", datetime(2025, 2, 28), datetime(2025, 3, 12)),
-    ("4.4.0-alpha.2", datetime(2025, 1, 16), datetime(2025, 2, 27)),
-    ("4.4.0-alpha.1", datetime(2024, 10, 8), datetime(2025, 1, 15)),
-    ("4.3.0-rc.1", datetime(2024, 10, 2), datetime(2024, 10, 7)),
-    ("4.3.0-beta.2", datetime(2024, 9, 18), datetime(2024, 10, 1)),
-    ("4.3.0-beta.1", datetime(2024, 8, 23), datetime(2024, 9, 17)),
-    ("4.3.0-alpha.5", datetime(2024, 7, 5), datetime(2024, 8, 22)),
+    ("4.4.0-alpha.4", datetime(2025, 3, 14), datetime(2026, 12, 31)),
+    ("4.4.0-alpha.3", datetime(2025, 2, 28), datetime(2025, 3, 13)),
+    ("4.4.0-alpha.2", datetime(2025, 1, 17), datetime(2025, 2, 27)),
+    ("4.4.0-alpha.1", datetime(2024, 10, 8), datetime(2025, 1, 16)),
+    ("4.3.0-rc.1", datetime(2024, 10, 1), datetime(2024, 10, 7)),
+    ("4.3.0-beta.2", datetime(2024, 9, 18), datetime(2024, 9, 30)),
+    ("4.3.0-beta.1", datetime(2024, 8, 24), datetime(2024, 9, 17)),
+    ("4.3.0-alpha.5", datetime(2024, 7, 5), datetime(2024, 8, 23)),
     ("4.3.0-alpha.4", datetime(2024, 5, 31), datetime(2024, 7, 4)),
     ("4.3.0-alpha.3", datetime(2024, 2, 17), datetime(2024, 5, 30)),
     ("4.3.0-alpha.2", datetime(2024, 2, 15), datetime(2024, 2, 17)),
